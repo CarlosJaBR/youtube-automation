7324a400460b154769c5b6cae417ef6f38dfd877
CarlosJaBR <carjabo12@gmail.com>
2026-03-30 08:25:06 -0500

    refactor: edit PlaySongStepDefinition class

--- src/test/java/com/youtube/stepDefinitions/PlaySongStepDefinition.java
+++ src/test/java/com/youtube/stepDefinitions/PlaySongStepDefinition.java
@@ -7,20 +7,15 @@ import io.cucumber.java.en.When;
 public class PlaySongStepDefinition {
     @Given("user open the browser")
     public void userOpenTheBrowser() {
-
-        throw new io.cucumber.java.PendingException();
     }
     @When("user send the video name")
     public void userSendTheVideoName() {
-        throw new io.cucumber.java.PendingException();
     }
     @When("user click on video")
     public void userClickOnVideo() {
-        throw new io.cucumber.java.PendingException();
     }
     @Then("user can see the title video")
     public void userCanSeeTheTitleVideo() {
-        throw new io.cucumber.java.PendingException();
     }
 
 }
