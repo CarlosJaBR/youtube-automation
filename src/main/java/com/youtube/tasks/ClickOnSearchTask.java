00edf628ece99f726bbaa0202cbda7b4a5167f9a
CarlosJaBR <carjabo12@gmail.com>
2026-04-10 20:50:38 -0500

    refactor: edit performAs method on ClickOnSearchTask

--- src/main/java/com/youtube/tasks/ClickOnSearchTask.java
+++ src/main/java/com/youtube/tasks/ClickOnSearchTask.java
@@ -3,14 +3,18 @@ package com.youtube.tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static com.youtube.ui.HomeUI.BTN_SEARCH;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class ClickOnSearchTask implements Task {
     @Override
     public <T extends Actor> void performAs(T t) {
         t.attemptsTo(
+                WaitUntil.the(BTN_SEARCH, isVisible()).
+                        forNoMoreThan(20).seconds(),
                 Click.on(BTN_SEARCH)
         );
     }
