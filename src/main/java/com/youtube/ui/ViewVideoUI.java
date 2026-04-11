565248231127a2f76c76f9232ed699f7f734f864
CarlosJaBR <carjabo12@gmail.com>
2026-04-10 21:09:25 -0500

    feat: add ViewVideoUI class

---
+++ src/main/java/com/youtube/ui/ViewVideoUI.java
@@ -0,0 +1,9 @@
+package com.youtube.ui;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class ViewVideoUI {
+    public static final Target LIST_TITTLEVIDEO=
+            Target.the("Title video")
+                    .locatedBy("yt-icon[@id='inline-tittle-icon']");
+}
