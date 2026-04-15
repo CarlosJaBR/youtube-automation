98cb5a33e1f6169ee310bb8993948d0de87a68cd
CarlosJaBR <carjabo12@gmail.com>
2026-04-14 23:28:24 -0500

    refactor: edit SelectVideo class

--- src/main/java/com/youtube/interactions/SelectVideo.java
+++ src/main/java/com/youtube/interactions/SelectVideo.java
@@ -4,8 +4,8 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.serenitybdd.screenplay.actions.Click;
-
 import java.util.ArrayList;
+import java.util.Random;
 
 import static com.youtube.ui.ViewVideoUI.LIST_TITTLEVIDEO;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
@@ -15,7 +15,9 @@ public class SelectVideo implements Interaction {
     @Override
     public <T extends Actor> void performAs(T t) {
         ArrayList<WebElementFacade> listVideos = LIST_TITTLEVIDEO.resolveAllFor(t);
-        WebElementFacade video = listVideos.get(0);
+        Random random = new Random();
+        int indexVideo = random.nextInt(listVideos.size());
+        WebElementFacade video = listVideos.get(indexVideo);
         t.attemptsTo(
                 Click.on(video)
         );
